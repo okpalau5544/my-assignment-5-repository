e0afa62e81aa13add25dfa380c0cb1de1949aab2
vivian55636 <okpalau@mcmaster.ca>
2025-06-25 01:27:35 +0000

    initial commit

--- src/routes/registerRoutes.ts
+++ src/routes/registerRoutes.ts
@@ -1,6 +1,14 @@
 import type Router from 'koa-router'
 // TODO: Update the path below to the correct file that exports RegisterRoutes
-import { RegisterRoutes } from './RegisterRoutes'
+// TODO: Update the path below to the correct file that exports RegisterRoutes
+// Update the import path below to the correct location of RegisterRoutes
+// Example: import { RegisterRoutes } from '../../generated/routes'
+// import { RegisterRoutes } from '../../generated/routes'
+// Update the import path below to the correct location of RegisterRoutes
+// Example: import { RegisterRoutes } from '../generated/routes'
+// Update the import path below to the correct location of RegisterRoutes
+// Example: import { RegisterRoutes } from '../../generated/routes'
+import { RegisterRoutes } from '../../generated/routes'
 
 export function registerRoutes (router: Router): void {
   RegisterRoutes(router)
