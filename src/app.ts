4b8f44ba8a292d23023f9025c8cb2ac986662372
vivian55636 <okpalau@mcmaster.ca>
2025-06-25 00:11:25 +0000

    initial commit

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
-import Koa from 'koa'; // Capital 'K', as per Koa docs
-import Router from 'koa-router';
-import bodyParser from 'koa-bodyparser';
-import { registerRoutes } from './routes/registerRoutes';
-import swaggerUi from 'swagger-ui-koa';
-import * as swaggerDocument from './swagger.json';
+import Koa from 'koa' // Capital 'K', as per Koa docs
+import Router from 'koa-router'
+import bodyParser from 'koa-bodyparser'
+import { registerRoutes } from './routes/registerRoutes'
+import swaggerUi from 'swagger-ui-koa'
+import * as swaggerDocument from './swagger.json'
 
-const app = new Koa();
-const router = new Router();
+const app = new Koa()
+const router = new Router()
 
-app.use(swaggerUi.serve);
-app.use(swaggerUi.setup(swaggerDocument));
-app.use(bodyParser());
-registerRoutes(router);
-app.use(router.routes()).use(router.allowedMethods());
+app.use(swaggerUi.serve)
+app.use(swaggerUi.setup(swaggerDocument))
+app.use(bodyParser())
+registerRoutes(router)
+app.use(router.routes()).use(router.allowedMethods())
 
-export default app;
+export default app
